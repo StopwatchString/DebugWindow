99a56512f94f7fddd651f4ca367cb5db4a7c734c
Mason Speck <mcs.5299@gmail.com>
2024-07-27 14:10:37 -0400

    comment

--- include/DebugWindow.h
+++ include/DebugWindow.h
@@ -20,6 +20,20 @@
 #include <string>
 #include <vector>
 
+/*
+    DebugWindow is meant to be a drop-in friendly class for random codebases with any kind of
+    frequently called loop. It encapsulates an Imgui instance and hides the cumbersome
+    implementation of managing a Win32 instance and deconflicting with any pre-existing OpenGL
+    instance.
+
+    Raw Win32 window management and C++ 14 adherence is to provide a lower chance of conflicting
+    with whatever state may already exist in applications this code is dropped into.
+
+    TODO: More fields from Imgui and Implot
+    TODO: Self-managed debug information, like automatically created timing graph based 
+          on how often draw() is called.
+*/
+
 class DebugWindow
 {
 public:
