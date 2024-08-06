49ce1bafde8ef2714762b2597b821282590d9a7f
Mason Speck <mcs.5299@gmail.com>
2024-08-05 21:50:03 -0400

    initial impl of glfw version

--- include/DebugWindowGLFW.h
+++ include/DebugWindowGLFW.h
@@ -1,5 +1,11 @@
 #include "DebugWindow.h"
 
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "GLFW/glfw3.h"
+
+static void glfw_error_callback(int error, const char* description);
+
 class DebugWindowGLFW : public DebugWindow
 {
 public:
@@ -10,4 +16,7 @@ protected:
     virtual void init() override;
     virtual void cleanup() override;
     virtual void drawImpl() override;
+
+private:
+    GLFWwindow* window{ nullptr };
 };
