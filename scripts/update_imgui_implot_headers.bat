f2c4af5f011cd76abdfffc80060624e043f2493f
Mason Speck <mcs.5299@gmail.com>
2024-08-11 19:54:38 -0400

    Updated interface so there is a single folder of include/ and src/

--- scripts/update_imgui_implot_headers.bat
+++ scripts/update_imgui_implot_headers.bat
@@ -1,30 +1,30 @@
 REM Imgui
 
-copy ..\extern\imgui\imconfig.h ..\include\imgui\imconfig.h
-copy ..\extern\imgui\imgui.h ..\include\imgui\imgui.h
-copy ..\extern\imgui\imgui_internal.h ..\include\imgui\imgui_internal.h
-copy ..\extern\imgui\imstb_rectpack.h ..\include\imgui\imstb_rectpack.h
-copy ..\extern\imgui\imstb_textedit.h ..\include\imgui\imstb_textedit.h
-copy ..\extern\imgui\imstb_truetype.h ..\include\imgui\imstb_truetype.h
+copy ..\extern\imgui\imconfig.h                           ..\include\DebugWindow\imconfig.h
+copy ..\extern\imgui\imgui.h                              ..\include\DebugWindow\imgui.h
+copy ..\extern\imgui\imgui_internal.h                     ..\include\DebugWindow\imgui_internal.h
+copy ..\extern\imgui\imstb_rectpack.h                     ..\include\DebugWindow\imstb_rectpack.h
+copy ..\extern\imgui\imstb_textedit.h                     ..\include\DebugWindow\imstb_textedit.h
+copy ..\extern\imgui\imstb_truetype.h                     ..\include\DebugWindow\imstb_truetype.h
 
-copy ..\extern\imgui\backends\imgui_impl_opengl3.h ..\include\imgui\imgui_impl_opengl3.h
-copy ..\extern\imgui\backends\imgui_impl_opengl3_loader.h ..\include\imgui\imgui_impl_opengl3_loader.h
-copy ..\extern\imgui\backends\imgui_impl_win32.h ..\include\imgui\imgui_impl_win32.h
-copy ..\extern\imgui\backends\imgui_impl_glfw.h ..\include\imgui\imgui_impl_glfw.h
+copy ..\extern\imgui\backends\imgui_impl_opengl3.h        ..\include\DebugWindow\imgui_impl_opengl3.h
+copy ..\extern\imgui\backends\imgui_impl_opengl3_loader.h ..\include\DebugWindow\imgui_impl_opengl3_loader.h
+copy ..\extern\imgui\backends\imgui_impl_win32.h          ..\include\DebugWindow\imgui_impl_win32.h
+copy ..\extern\imgui\backends\imgui_impl_glfw.h           ..\include\DebugWindow\imgui_impl_glfw.h
 
-copy ..\extern\imgui\imgui.cpp ..\src\imgui\imgui.cpp
-copy ..\extern\imgui\imgui_draw.cpp ..\src\imgui\imgui_draw.cpp
-copy ..\extern\imgui\imgui_tables.cpp ..\src\imgui\imgui_tables.cpp
-copy ..\extern\imgui\imgui_widgets.cpp ..\src\imgui\imgui_widgets.cpp
+copy ..\extern\imgui\imgui.cpp                            ..\src\DebugWindow\imgui.cpp
+copy ..\extern\imgui\imgui_draw.cpp                       ..\src\DebugWindow\imgui_draw.cpp
+copy ..\extern\imgui\imgui_tables.cpp                     ..\src\DebugWindow\imgui_tables.cpp
+copy ..\extern\imgui\imgui_widgets.cpp                    ..\src\DebugWindow\imgui_widgets.cpp
 
-copy ..\extern\imgui\backends\imgui_impl_opengl3.cpp ..\src\imgui\imgui_impl_opengl3.cpp
-copy ..\extern\imgui\backends\imgui_impl_win32.cpp ..\src\imgui\imgui_impl_win32.cpp
-copy ..\extern\imgui\backends\imgui_impl_glfw.cpp ..\src\imgui\imgui_impl_glfw.cpp
+copy ..\extern\imgui\backends\imgui_impl_opengl3.cpp      ..\src\DebugWindow\imgui_impl_opengl3.cpp
+copy ..\extern\imgui\backends\imgui_impl_win32.cpp        ..\src\DebugWindow\imgui_impl_win32.cpp
+copy ..\extern\imgui\backends\imgui_impl_glfw.cpp         ..\src\DebugWindow\imgui_impl_glfw.cpp
 
 REM Implot
 
-copy ..\extern\implot\implot.h ..\include\imgui\implot.h
-copy ..\extern\implot\implot_internal.h ..\include\imgui\implot_internal.h
+copy ..\extern\implot\implot.h                            ..\include\DebugWindow\implot.h
+copy ..\extern\implot\implot_internal.h                   ..\include\DebugWindow\implot_internal.h
 
-copy ..\extern\implot\implot.cpp ..\src\imgui\implot.cpp
-copy ..\extern\implot\implot_items.cpp ..\src\imgui\implot_items.cpp
+copy ..\extern\implot\implot.cpp                          ..\src\DebugWindow\implot.cpp
+copy ..\extern\implot\implot_items.cpp                    ..\src\DebugWindow\implot_items.cpp
